d962fb655dd9d2447db3cf58c466d2ad8323b5b6
Constanza Vázquez <cvazqueztes@gmail.com>
2025-10-25 21:59:54 -0300

    Output positions of '0' characters in string
    Replaces previous logic that printed string length and indices with code that collects and outputs the positions of '0' characters in the input string, as required by the problem statement.

--- Codeforces/Codeforces_Round_1059_div_3/b.cpp
+++ Codeforces/Codeforces_Round_1059_div_3/b.cpp
@@ -13,8 +13,16 @@ int main()
         string s;
         cin >> s;
         
-        cout << s.length() << endl;
-        for(int i = 1; i <= s.length(); i++) cout << i << " ";
+        vector<int> mish;
+        
+        for(int a = 0; a < s.length(); a++){
+            if(s[a] == '0') mish.push_back(a+1);
+        }
+        
+        cout << mish.size() << endl;
+        for(int i = 0; i < mish.size(); i++){
+            cout << mish[i] << " ";
+        }
         cout << endl;
     }
 
